407f94f0a66313a1d5fce8588581f814d364ad6d
Daniele Pallastrelli <5451767+daniele77@users.noreply.github.com>
2024-05-31 17:09:29 +0200

    Move pragmas to disable gcc warning in a special header file

--- src/examples/static.cpp
+++ src/examples/static.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <asio.hpp> // NB: the asio header must be included *before* iostream to avoid sanity check error
+#include "f16asio.hpp" // NB: the asio header must be included *before* iostream to avoid sanity check error
 #include <iostream>
 #include "server.hpp"
 #include "static_content.hpp"
