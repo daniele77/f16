14889a7738d55756691e1f4172ea455e38e6541f
Daniele Pallastrelli <daniele.pallastrelli@sadel.it>
2024-08-16 16:33:02 +0200

    plain_connection and ssl_connection share code in base_connection

--- src/lib/plain_connection.cpp
+++ src/lib/plain_connection.cpp
@@ -13,78 +13,13 @@ namespace f16::http::server {
 
 plain_connection::plain_connection(asio::ip::tcp::socket socket,
     connection_manager& manager, request_handler& handler)
-  : socket_(std::move(socket)),
-    connection_manager_(manager),
-    request_handler_(handler),
-    buffer_{},
-    request_{},
-    reply_{}
+  : base_connection(std::move(socket), manager, handler)
 {
 }
 
 void plain_connection::start()
 {
   do_read();
-}
-
-void plain_connection::stop()
-{
-  socket_.close();
-}
-
-void plain_connection::do_read()
-{
-  auto self(shared_from_this());
-  socket_.async_read_some(asio::buffer(buffer_),
-      [this, self](std::error_code ec, std::size_t bytes_transferred)
-      {
-        if (!ec)
-        {
-          request_parser::result_type result = request_parser::bad;
-          std::tie(result, std::ignore) = request_parser_.parse(
-              request_, buffer_.begin(), buffer_.begin() + bytes_transferred);
-
-          if (result == request_parser::good)
-          {
-            request_handler_.handle_request(request_, reply_);
-            do_write();
-          }
-          else if (result == request_parser::bad)
-          {
-            reply_ = reply::stock_reply(reply::bad_request);
-            do_write();
-          }
-          else
-          {
-            do_read();
-          }
-        }
-        else if (ec != asio::error::operation_aborted)
-        {
-          connection_manager_.stop(shared_from_this());
-        }
-      });
-}
-
-void plain_connection::do_write()
-{
-  auto self(shared_from_this());
-  asio::async_write(socket_, reply_.to_buffers(),
-      [this, self](std::error_code ec, std::size_t)
-      {
-        if (!ec)
-        {
-          // Initiate graceful plain_connection closure.
-          asio::error_code ignored_ec;
-          socket_.shutdown(asio::ip::tcp::socket::shutdown_both,
-            ignored_ec);
-        }
-
-        if (ec != asio::error::operation_aborted)
-        {
-          connection_manager_.stop(shared_from_this());
-        }
-      });
 }
 
 } // namespace f16::http::server
